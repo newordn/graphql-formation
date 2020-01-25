0dca34464a4c3cc63683d4e26bb81982d6bf451e
neword <newordn@gmail.com>
2020-01-25 16:16:12 +0100

    Ajout de creation post et listing posts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { GraphQLServer } = require('graphql-yoga')
 const utilisateurs = []
-
+const posts = []
+let estConnecte = false
 const typeDefs = `
 type Query {
   utilisateurs: [User]
@@ -12,37 +13,71 @@ type Post{
   contenu:String
   statut:Boolean
   createdAt: String
+  postedBy: User
 }
 type Mutation{
   validationPost(id:ID!):Post
   creationPost(sujet:String,contenu:String): Post
   inscription(noms:String,mot_de_passe:String,date_naissance:String): User
-  connexion(noms:String,mot_de_passe:String): Boolean
+  connexion(noms:String,mot_de_passe:String): User
 }
 type User{
+  id: ID!
   noms:String
   mot_de_passe:String
   date_naissance:String
+  posts: [Post]
   createdAt: String
 }
 `
-
+const dateActuelle = ()=>{
+  const date = new Date() // on cree un objet qui contient la date du jour
+return date.getFullYear().toString() + "-0"+ (date.getMonth()+1).toString() + "-"+ date.getDate().toString()
+}
+const getUserById = (id)=>{
+  //utilisateurs.map(user=>user.id===id)
+ const user = utilisateurs.filter(user=>user.id===id)[0]
+ return user
+}
 const resolvers = {
   Query: {
     info: () => {console.log('on est dans la fonction info'); return `This is the api for graphql-formation`},
-    utilisateurs: ()=>utilisateurs
+    utilisateurs: ()=>utilisateurs,
+    posts: ()=>{
+      return posts.filter(post=>posts.statut===true) 
+     }
   },
   Mutation:{
     inscription: (parent,args,context,info)=>{
       console.log("dans inscription")
       const user = args// recupere les informations de l'utilisateur
-      const date = new Date() // on cree un objet qui contient la date du jour
-     // construit la date a afficher a l'utilisateur
-      user.createdAt = date.getFullYear().toString() + "-0"+ (date.getMonth()+1).toString() + "-"+ date.getDate().toString()
+       // construit la date a afficher a l'utilisateur
+     user.id= new Date().getTime() 
+     user.posts = []
+     user.createdAt = dateActuelle()
      // ajoute le nouvel utilisateur a la liste des utilisateurs
       utilisateurs.push(user)
       // retourne le premier utilisateur
       return utilisateurs[utilisateurs.length-1]
+    },
+    creationPost: (parent,args,context,info)=>{
+      const post = {...args, statut:false,createdAt: dateActuelle()}
+       
+      // verifie si l'utilisateur est connecte
+        if(estConnecte)
+        {
+          console.log(estConnecte) // affichage de l'id de l'utilisateur connecte
+          post.postedBy = getUserById(estConnecte)
+        // si oui on recupere l'id de l'utilisateur connecte
+        }
+        // si non il se doit se connecter d'abord
+        else{
+          throw "Connectez vous d'abord avant de pouvoir poster"
+        }
+        // on cree le post
+        posts.push(post)
+        return post
+
     },
     connexion: (parent,args,context,info)=>{
       // parse les donnees 
@@ -54,17 +89,18 @@ const resolvers = {
             console.log("l'user est chez nouss")
             if(utilisateurs[i].mot_de_passe===password){
                 console.log('il est connecte')
-                return true
+                estConnecte =  utilisateurs[i].id
+                return utilisateurs[i]
             }
             else{
                 console.log('le mot de passe ne correspond pas')
-                return false
+                throw "Mot de passe incorrect"
             }
         }  
         else if(i===utilisateurs.length-1)
         {
             console.log("l'utilisateur n'est pas chez nous")
-            return false
+            throw "Utilisateur n'existe pas"
         }
         }
       }
